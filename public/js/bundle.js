41478cb784d3f5dde8a5aaf43e03160fbfcd2c8e
Mike Skalandunas <mskalandunas@gmail.com>
2017-03-14 21:32:22 -0700

    add move function

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -70,28 +70,60 @@
 	    this.container = this.node.querySelector('.carousel-node-container');
 	    this.caption   = this.node.querySelector('.carousel-caption');
 	    this.anchor    = this.node.querySelector('.carousel-anchor');
+	    this.previous  = this.node.querySelector('.carousel-previous');
+	    this.next      = this.node.querySelector('.carousel-next');
 	    this.built     = false;
 
-	    this.container.addEventListener('click', this.active.bind(this), true);
+	    this.container.addEventListener('click', this.move.bind(this), true);
 	    this.build();
-	    this.active();
+	    this.move();
 	    this.built     = true;
 	  };
 
-	  active(e) {
+	  build() {
+	    for (let i = 0, len = this.o.length; i < len; i++) {
+	      let node = document.createElement('div');
+
+	      node.setAttribute('class', 'carousel-node');
+	      node.dataset.position = i;
+	      this.container.appendChild(node);
+	    };
+
+	    if (this.previous != null) {
+	      this.previous.addEventListener('click', this.move.bind(this), true);
+	    };
+
+	    if (this.next != null) {
+	      this.next.addEventListener('click', this.move.bind(this), true);
+	    };
+
+	    this.container.children[0].classList.add('active-node');
+	  };
+
+	  move(e) {
 	    let that;
 	    let o;
 
-	    if (!this.built) {
-	      that = this.container.children[0];
-	      o    = this.o[0];
-	    } else {
-	      that = e.srcElement
-	      o    = this.o[that.dataset.position];
+	    if (e && e.srcElement.classList.contains('carousel-node-container')) {
+	      return;
 	    };
 
-	    if (e && !e.srcElement.classList.contains('carousel-node')) {
-	      return;
+	    if (!this.built) {
+	      that  = this.container.children[0];
+	      o     = this.o[0];
+	    } else if (e.srcElement.classList.contains('carousel-node')) {
+	      that  = e.srcElement;
+	      o     = this.o[that.dataset.position];
+	    } else if (e.srcElement.classList.contains('carousel-next')) {
+	      let c = +this.node.querySelector('.active-node').dataset.position + 1;
+	      let l = this.o.length;
+	      that  =  c < l ? this.node.querySelector(`[data-position='${c}']`) : this.node.querySelector(`[data-position='0']`);
+	      o     = this.o[that.dataset.position];
+	    } else if (e.srcElement.classList.contains('carousel-previous')) {
+	      let c = +this.node.querySelector('.active-node').dataset.position - 1;
+	      let l = this.o.length;
+	      that  =  c >= 0 ? this.node.querySelector(`[data-position='${c}']`) : this.node.querySelector(`[data-position='${l - 1}']`);
+	      o     = this.o[that.dataset.position];
 	    };
 
 	    if (o.hasOwnProperty('title')) {
@@ -109,18 +141,6 @@
 	    this.img.setAttribute('src', o.src);
 	    this.container.querySelector('.active-node').classList.remove('active-node');
 	    that.classList.add('active-node');
-	  };
-
-	  build() {
-	    for (let i = 0, len = this.o.length; i < len; i++) {
-	      let node = document.createElement('div');
-
-	      node.setAttribute('class', 'carousel-node');
-	      node.dataset.position = i;
-	      this.container.appendChild(node);
-	    };
-
-	    this.container.children[0].classList.add('active-node');
 	  };
 	};
 
